9450f7f07fb7d5e5996b1ccde4da0ccda298b36b
Goldleader17 <jc6364972@gmail.com>
2023-12-17 23:16:21 +0100

    reaction-timer game source code

---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>Reaction Time: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if(this.score < 250) {
+      this.rank = 'Ninja Fingers'
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = 'Snail Pace...'
+    }
+  }
+}
+</script>
+
+<style>
+  .rank{
+    font-size: 1.4rem;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
